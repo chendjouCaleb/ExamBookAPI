6514349ed92a8d83e942893d5d9d7604f88795fe
chendjou2016 <chendjou2016@outlook.fr>
2023-07-23 07:53:39 +0100

    test service interface.

---
+++ ExamBook/Services/TestTeacherService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExamBook.Entities;
+using ExamBook.Models;
+using ExamBook.Persistence;
+
+namespace ExamBook.Services
+{
+	public class TestTeacherService
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+
+		public async Task<CourseTeacher> GetAsync(ulong courseTeacherId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<bool> ContainsAsync(Test test, Member member)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ActionResultModel<CourseTeacher>> AddCourseTeacher(Test test, Member member)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<CourseTeacher> CreateAsync(Test test, Member member)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task DeleteAsync(TestTeacher testTeacher)
+		{
+			throw new NotImplementedException();	
+		}
+	}
+}
